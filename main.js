7ee52f08d85b927ce7271555fe767c8faaeb5c7f
Kevins Villatoro <kevinnz36@gmail.com>
2024-01-22 03:48:33 -0300

    Corrección de lógica, corrección de estilos, ordenamiento de codigo js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-import './style.css'
+//TETRIS GAME
+import './style.css';
 
-
-//Dimensionamiento del tablero
+// Dimensionamiento del tablero
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
-const score = document.querySelector('span')
+const score = document.querySelector('span');
+const quitButton = document.getElementById('quit-button');
 
+let audio = null;
 
 const blockSize = 20;
 const boardWidth = 14;
@@ -13,69 +15,42 @@ const boardHeight = 30;
 
 let scoreV = 0;
 
-
 canvas.width = blockSize * boardWidth;
 canvas.height = blockSize * boardHeight;
 
 context.scale(blockSize, blockSize);
 
-
-//Tablero
+// Módulo: Tablero
 const board = createBoard(boardWidth, boardHeight);
 
 function createBoard(width, height) {
-  return Array(height).fill().map(() => Array(width).fill(0))
+  return Array(height).fill().map(() => Array(width).fill(0));
 }
 
-//Piezas
-
+// Módulo: Piezas
 const piece = {
   position: { x: 5, y: 5 },
   shape: [[1, 1], [1, 1]]
-}
+};
 
 const pieces = [
-  [
-    [1, 1],
-    [1, 1]
-  ], 
-  [
-    [1, 1, 1, 1]
-  ],
-  [
-    [0, 1, 0],
-    [1, 1, 1]
-  ], 
-  [
-    [1, 1, 0],
-    [0, 1, 1]
-  ],
-  [
-    [0, 1, 1],
-    [1, 1, 0]
-  ],
-  [
-    [1, 0],
-    [1, 0],
-    [1, 1]
-  ], 
-  [
-    [0, 1],
-    [0, 1],
-    [1, 1]
-  ]
-]
+  [[1, 1], [1, 1]],
+  [[1, 1, 1, 1]],
+  [[0, 1, 0], [1, 1, 1]],
+  [[1, 1, 0], [0, 1, 1]],
+  [[0, 1, 1], [1, 1, 0]],
+  [[1, 0], [1, 0], [1, 1]],
+  [[0, 1], [0, 1], [1, 1]]
+];
 
-//Función que dibuja y actualiza el tablero 
-
+// Módulo: Dibujo y actualización del tablero
 let drop = 0;
 let lastTime = 0;
 
 function refresh(time = 0) {
-  console.log('Refresh function called.');
   const deltaTime = time - lastTime;
   lastTime = time;
-  drop += deltaTime
+  drop += deltaTime;
 
   if (drop > 1000) {
     piece.position.y++;
@@ -87,22 +62,21 @@ function refresh(time = 0) {
       cleanRows();
     }
   }
-  draw()
-  window.requestAnimationFrame(refresh)
+  draw();
+  window.requestAnimationFrame(refresh);
 }
 
-
 function draw() {
-  context.fillStyle = '#000'
-  context.fillRect(0, 0, canvas.width, canvas.height)
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
   board.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value === 1) {
-        context.fillStyle = 'yellow'
+        context.fillStyle = 'yellow';
         context.fillRect(x, y, 1, 1);
       }
-    })
+    });
   });
 
   piece.shape.forEach((row, y) => {
@@ -116,46 +90,52 @@ function draw() {
   score.innerText = scoreV;
 }
 
-
+// Módulo: Colisión
 function checkCollision() {
   return piece.shape.find((row, y) => {
     return row.find((value, x) => {
-      return (
+      const newX = x + piece.position.x;
+      const newY = y + piece.position.y;
+
+      if (
         value !== 0 &&
-        board[y + piece.position.y]?.[x + piece.position.x] !== 0
-      );
-    })
-  })
+        (newX < 0 || newX >= boardWidth || newY >= boardHeight || newY < 0 ||
+          board[newY]?.[newX] !== 0)
+      ) {
+        return true;
+      }
+
+      return false;
+    });
+  });
 }
 
+// Módulo: Reinicio de pieza
 function resetPiece() {
   piece.position.x = Math.floor(boardWidth / 2 - 2);
   piece.position.y = 0;
-  piece.shape = pieces[Math.floor(Math.random() * pieces.length)]
+  piece.shape = pieces[Math.floor(Math.random() * pieces.length)];
 
   if (checkCollision()) {
     window.alert('Fin del juego');
-    board.forEach((row) => row.fill(0))
+    board.forEach((row) => row.fill(0));
   }
-
-
 }
 
-//Crear Pieza(acoplar)
+// Módulo: Crear Pieza (acoplar)
 function createPiece() {
   piece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value === 1) {
         board[y + piece.position.y][x + piece.position.x] = 1;
       }
-    })
-  })
+    });
+  });
 
   resetPiece();
 }
 
-//Limpiar lineas
-
+// Módulo: Limpiar líneas
 function cleanRows() {
   const rowsToDelete = [];
 
@@ -163,77 +143,106 @@ function cleanRows() {
     if (row.every(value => value === 1)) {
       rowsToDelete.push(y);
     }
-  })
+  });
+
+  if (rowsToDelete.length > 0) {
+    scoreV += 10 * Math.pow(2, rowsToDelete.length - 1);
+  }
 
   rowsToDelete.forEach(y => {
     board.splice(y, 1);
     const newRow = Array(boardWidth).fill(0);
     board.unshift(newRow);
-    scoreV += 10;
-  })
+  });
 }
 
-//Movimiento de las piezas
+// Módulo: Fin del juego
+function endGame() {
+  audio.pause();
+  board.forEach((row) => row.fill(0));
+  scoreV = 0;
+  draw();
+  refresh();
+  window.alert('Juego finalizado');
+  window.location.reload();
+}
 
+// Módulo: Movimiento de las piezas
 document.addEventListener('keydown', event => {
   if (event.key === 'ArrowLeft') {
-    piece.position.x--
+    piece.position.x--;
     if (checkCollision()) {
-      piece.position.x++
-    };
-  };
+      piece.position.x++;
+    }
+  }
 
   if (event.key === 'ArrowRight') {
-    piece.position.x++
+    piece.position.x++;
     if (checkCollision()) {
       piece.position.x--;
     }
-  };
+  }
 
   if (event.key === 'ArrowDown') {
-    piece.position.y++
+    piece.position.y++;
     if (checkCollision()) {
       piece.position.y--;
       createPiece();
       cleanRows();
     }
-  };
+  }
 
   if (event.key === 'ArrowUp') {
-    const rotated = []
+    const rotated = [];
 
     for (let i = 0; i < piece.shape[0].length; i++) {
-      const row = []
+      const row = [];
 
       for (let j = piece.shape.length - 1; j >= 0; j--) {
-
-        row.push(piece.shape[j][i])
-
+        row.push(piece.shape[j][i]);
       }
-      rotated.push(row)
+      rotated.push(row);
     }
-    const previousShape = piece.shape
-    piece.shape = rotated
+    const previousShape = piece.shape;
+    piece.shape = rotated;
     if (checkCollision()) {
-      piece.shape = previousShape
+      piece.shape = previousShape;
     }
   }
 });
 
+// Evento de clic para el botón de salir
+quitButton.addEventListener('click', () => {
+  endGame();
+});
 
+// Evento de clic para iniciar/reiniciar el juego
 document.querySelector('section').addEventListener('click', () => {
-  refresh()
-  const audio = new Audio('./public/Tetris.mp3')
-  audio.volume = 0.3
-  audio.addEventListener('ended', () => {
-    audio.currentTime = 0.1; 
-    audio.play(); 
-  });
+  refresh();
 
-  audio.play();
+  if (!audio) {
+    audio = new Audio('./public/Tetris.mp3');
+    audio.volume = 0.3;
+    audio.addEventListener('ended', () => {
+      audio.currentTime = 0.1;
+      audio.play();
+    });
 
+    audio.play();
+  } else {
+    endGame();
+  }
+});
 
-})
+// Evento de clic para el botón de silenciar
+const muteButton = document.getElementById('mute-button');
 
-
-
+muteButton.addEventListener('click', () => {
+  if (audio) {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+});
